a1f273091c74f7f3e1a20bf5a2d28a32c003dec4
maverickquill55 <VScode@toxik.cf>
2022-09-27 21:03:31 -0400

    return to function

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import { Server as Sock } from "socket.io"
 import config from "./config.js"
 
-export default {
-    name: `realtime`,
-    async configureServer(serv) {
-        console.log('config')
-        const io = new Sock(serv.httpServer)
-        (await config).mount(io, serv)
+export default function plugin() {
+    return{
+        name: `realtime`,
+        async configureServer(serv) {
+            console.log('config')
+            const io = new Sock(serv.httpServer)
+            (await config).mount(io, serv)
+        }
     }
 }
 export function defineConfig(config) {return config}
